08d750732db4872fbe2ad28f2cd265678a9b8d92
Vinay288 <vinayhiremath288@gmail.com>
2021-09-13 12:57:42 +0530

    MODIFIED: number playlist class added lambda funtion to print the element using foreach

--- lib/src/main/java/MathOperationApp/NumberPlayList.java
+++ lib/src/main/java/MathOperationApp/NumberPlayList.java
@@ -6,20 +6,26 @@ import java.util.function.Consumer;
 public class NumberPlayList {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> myNumberList=new ArrayList<Integer>();
-		for(int i=0;i<5;i++)myNumberList.add(i);
-		
-		for(int i:myNumberList)
-			System.out.println("numbers are"+i);
-	
-		class MyConsumer implements Consumer<Integer>{
-		public void accept(Integer t) {
-			System.out.println("values of list by implementing consumer class: "+t);
-		};
+		ArrayList<Integer> myNumberList = new ArrayList<Integer>();
+		for (int i = 0; i < 5; i++)
+			myNumberList.add(i);
+
+		for (int i : myNumberList)
+			System.out.println("numbers are" + i);
+		// explicit lambda fucntion
+		Consumer<Integer> myListAction = n -> System.out.println("lambda impl foreach value :" + n);
+		myNumberList.forEach(myListAction);
+
+		// implicit lambda function
+		myNumberList.forEach(n -> System.out.println("values are: " + n));
+
+		class MyConsumer implements Consumer<Integer> {
+			public void accept(Integer t) {
+				System.out.println("values of list using class: " + t);
+			};
 		}
 		MyConsumer action = new MyConsumer();
 		myNumberList.forEach(action);
 	}
 
 }
-
