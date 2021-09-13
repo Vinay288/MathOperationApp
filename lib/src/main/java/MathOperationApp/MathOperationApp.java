36768d0008215c75f7382c5ff9c9a1e730a52925
Vinay288 <vinayhiremath288@gmail.com>
2021-09-13 11:43:13 +0530

    ADDED: static method inside functional interface to show result by passing lambda functions

--- lib/src/main/java/MathOperationApp/MathOperationApp.java
+++ lib/src/main/java/MathOperationApp/MathOperationApp.java
@@ -6,6 +6,9 @@ package MathOperationApp;
 @FunctionalInterface
 interface IMathFunction {
 	int calculate(int a, int b);
+	static void printResult(int a,int b,String functionName,IMathFunction functionObj) {
+		System.out.println(functionName+" is  "+functionObj.calculate(a, b));
+	}
 }
 
 public class MathOperationApp {
@@ -14,9 +17,9 @@ public class MathOperationApp {
 		IMathFunction multiply = (a, b) -> a * b;
 		IMathFunction divide = (int a, int b) -> a / b;
 		IMathFunction subtract = (int a, int b) -> a - b;
-		System.out.println("addition " + add.calculate(2, 3));
-		System.out.println("multiplication " + multiply.calculate(2, 3));
-		System.out.println("subtraction " + subtract.calculate(2, 3));
-		System.out.println("division " + divide.calculate(2, 3));
+		IMathFunction.printResult(2, 3, "addition", add);
+		IMathFunction.printResult(3, 2, "multiply", multiply);
+		IMathFunction.printResult(5, 3, "subtract", subtract);
+		IMathFunction.printResult(4, 2, "dividion", divide);
 	}
 }
